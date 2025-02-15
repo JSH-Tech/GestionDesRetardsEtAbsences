f22e04cde58c081e71ea8da375ced8162c05a359
JSH-Tech <josueabots@gmail.com>
2025-02-14 21:48:04 -0500

    Regeneration des classe apres modification de la base de donnee

---
+++ GestionDesRetardsEtAbseneces/Model/Authentification.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GestionDesRetardsEtAbseneces.Model;
+
+public partial class Authentification
+{
+    /// <summary>
+    /// Primary Key
+    /// </summary>
+    public int IdAuthentification { get; set; }
+
+    /// <summary>
+    /// Foreign Key
+    /// </summary>
+    public int IdEmploye { get; set; }
+
+    /// <summary>
+    /// Hashed Password
+    /// </summary>
+    public string MotDePasseHash { get; set; } = null!;
+
+    /// <summary>
+    /// Last Connection Date
+    /// </summary>
+    public DateTime DateDerniereConnexion { get; set; }
+
+    /// <summary>
+    /// Session Token
+    /// </summary>
+    public string TokenSession { get; set; } = null!;
+
+    /// <summary>
+    /// Expiration Date
+    /// </summary>
+    public DateTime DateExpiration { get; set; }
+
+    /// <summary>
+    /// Access Level
+    /// </summary>
+    public int NiveauAcces { get; set; }
+
+    public virtual Employe IdEmployeNavigation { get; set; } = null!;
+}
